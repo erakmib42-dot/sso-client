5469181d42249cd122f2afd103ef286b461ad83a
yermek <erakmib42@gmail.com>
2025-12-04 16:10:36 +0500

    add view components

---
+++ src/View/Components/Button.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace USO\SsoClient\View\Components;
+
+use Illuminate\View\Component;
+
+class Button extends Component
+{
+    public function render()
+    {
+        return view('sso::components.button');
+    }
+}

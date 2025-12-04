45e96653be939900c49f482c1f50770981a49cfd
yermek <erakmib42@gmail.com>
2025-12-04 14:45:02 +0500

    Laravel SSO packages

--- src/SSOServiceProvider.php
+++ src/SSOServiceProvider.php
@@ -22,7 +22,7 @@ class SSOServiceProvider extends ServiceProvider
             $this->publishes([ __DIR__.'/../config/sso.php' => config_path('sso.php') ], 'config');
         }
 
-        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
 
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'sso');
         Blade::componentNamespace('USO\\SsoClient\\View\\Components', 'sso');
